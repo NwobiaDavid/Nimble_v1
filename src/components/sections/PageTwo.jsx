0dbf5787cd15964f87b3064da6c906ccf13a2b45
NwobiaDavid <dnwobia@gmail.com>
2023-10-07 00:08:23 +0100

    pagetwo done

--- src/components/sections/PageTwo.jsx
+++ src/components/sections/PageTwo.jsx
@@ -1,8 +1,14 @@
 
 export default function PageTwo() {
   return (
-    <div>
-      PageTwo
+    <div className="flex flex-col items-center justify-center h-[680px]  ">
+      <div className="flex p-3 border border-black w-[20%] items-center justify-center rounded-md text-3xl font-semibold ">
+        <h2>What is Nimble?</h2>
+      </div>
+      <div className="flex flex-col mt-14 text-2xl items-center justify-center ">
+        <h2 className="font-bold mb-2 ">Your Personal AI Companion</h2>
+        <p>‍Nimble is more than just an app – it's your go-to assistant for managing tasks, enhancing communication, and making life more streamlined.</p>
+      </div>
     </div>
   )
 }
