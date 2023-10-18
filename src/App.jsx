f8e7eb91f54f8f0e37713f18bc4435d61c00de37
NwobiaDavid <dnwobia@gmail.com>
2023-10-18 11:53:02 +0100

    added a header for thenumbered component

--- src/App.jsx
+++ src/App.jsx
@@ -10,35 +10,36 @@ import Features from './components/sections/Features';
 import One from './components/sections/solutions/One';
 import Two from './components/sections/solutions/Two';
 
-import {useEffect} from "react";
+import { useEffect } from 'react';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import Three from './components/sections/solutions/Three';
 
 function App() {
-
   useEffect(() => {
     AOS.init();
     AOS.refresh();
-  }, [])
-
+  }, []);
 
   return (
     <>
       <div>
-      <NavBar />
-       <div className="px-10 py-20">
-         <HomePage />
-         <Features />
-         <div className='h-[1080px] py-10 flex flex-col justify-evenly'><One/> <Two/> <Three /> </div>
-         <div data-aos="fade-up">
-           <PageTwo />
-         </div>
-         <PageThree />
-         <PageFour />
-         <Contact />
-       </div>
-       <Footer />
+        <NavBar />
+        <div className="px-10 py-20">
+          <HomePage />
+          <Features />
+          <div className="h-[1200px] py-10 pt-16 flex flex-col items-center justify-evenly">
+          <div className='text-2xl mt-20 mb-3 w-[20%] border rounded-xl border-black uppercase p-3 font-bold text-center font-nunito '>nimble&rsquo;s features</div>
+            <One /> <Two /> <Three />
+          </div>
+          <div data-aos="fade-up">
+            <PageTwo />
+          </div>
+          <PageThree />
+          <PageFour />
+          <Contact />
+        </div>
+        <Footer />
       </div>
     </>
   );
